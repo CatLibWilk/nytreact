cefdbcd77422140cf9676f99655103853ca490c5
Andy Wilk <andrewdwilk@gmail.com>
2018-10-03 13:07:29 -0400

    added styling

--- client/src/components/Buttons/DelBtn/DelBtn.js
+++ client/src/components/Buttons/DelBtn/DelBtn.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const DelBtn = (props) => (
-  <div name="DelBtn" className="btn btn-primary float-left" {...props}>Remove</div>
+  <div name="DelBtn" className="btn btn-primary float-left mx-auto mt-1" {...props}>Remove</div>
 );
 
 export default DelBtn;
--- client/src/components/Buttons/SaveBtn/SaveBtn.js
+++ client/src/components/Buttons/SaveBtn/SaveBtn.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SaveBtn = (props) => (
-  <div name="SaveBtn" className="btn btn-primary float-left" {...props}>Save</div>
+  <div name="SaveBtn" className="btn btn-primary mx-auto mt-1" {...props}>Save</div>
 );
 
 export default SaveBtn;
